81a9c8fb5cfab964769cb8d980ea7581529b52dc
Pierrick Lebourgeois <pierrick@trent.homelinux.org>
2014-08-18 22:03:52 +0200

    indique si l'adversaire a joué ou pas dans le mail de notification

--- move.py
+++ move.py
@@ -127,6 +127,9 @@ if __name__ == "__main__":
 	logging.debug('sujet : ' + sujet)
 	
 	msg = open('conf/mail_notif.txt').read() % (login, coup, txt, dico_params['com'], url)
+	if coup == '':
+		msg = msg.replace('a joué :', "n'a pas joué")
+	
 	r = mail.send_mail(email, sujet, msg )
 	b.add_move(dico_params['gid'], dico_params['c'])
 	#~ test local
