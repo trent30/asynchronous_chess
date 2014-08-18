91aed4454eead64e8b6d512684db0feccb6d02fa
Pierrick Lebourgeois <pierrick@trent.homelinux.org>
2014-08-18 21:56:26 +0200

    [bugfix] '\n' après le header dans logout.py

--- logout.py
+++ logout.py
@@ -6,7 +6,6 @@ import cgi
 import bdd
 from cookie_check import get_cookie
 
-	
 if __name__ == "__main__":
 	print "Content-type: text/html"
 	
@@ -21,12 +20,12 @@ if __name__ == "__main__":
 		s = c["session"].value
 	except:
 		s = None
-		print "Vous n'êtes pas connecté."
+		print "\nVous n'êtes pas connecté."
 		exit(0)
 		
 	b = bdd.bdd()
 	if not b.autorized(s):
-		print "Votre session a expirée. Veuillez vous reconnecter (pensez à autoriser les cookies si ce n'est pas le cas)."
+		print "\nVotre session a expirée. Veuillez vous reconnecter (pensez à autoriser les cookies si ce n'est pas le cas)."
 		exit(0)
 	
 	b.delete_cookie(s)
