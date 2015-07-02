b077b664f806f2c0343b099604dc9b9d065cb1c7
Pierrick Lebourgeois <pierrick@trent.homelinux.org>
2015-07-02 21:33:15 +0200

    La liste des joueurs est triable par nom ou par elo.

--- main.js
+++ main.js
@@ -1251,24 +1251,99 @@ function back_stats() {
 	l.innerHTML = L_stats;
 }
 
-function players_return(r) {
-	var l = $('log');
-	var j = JSON.parse(r);
-	var e = '<p>Cliquez sur le nom du joueur pour lui proposer une partie ou cliquez sur le graphique pour voir ses statistiques (utilisez CTRL+F pour faire une recherche) </p>';
-	if (j.length == 0) {
-		e += "<p>Aucun joueur<p/>";
-	} else {
-		for (var i in j) {
-			if (j[i].nom == user_ID) {
-				//~ on en profite pour mettre à jour le elo du joueur
-				try_set_local('elo', j[i].elo);
-			}
-			var p = "onclick=invite(" + j[i].id + ",'" + j[i].nom + "');";
-			e += "<div class='lst_players'><img class='lst_players' src='./img/stats.png' onclick='get_stats(" + j[i].id + ")'></div><div class='player lst_players' id=" + j[i].id + " " + p + ">  " + j[i].nom + " ( " + j[i].elo + " )</div><div></div>";
+function sort_players(key, asc) {
+	add_log('tri en cours...');
+	liste = [];
+	//~ création d'une liste des 'key'
+	for (var i in ALL_PLAYERS) {
+		if (key == 'nom') {
+			//~ mettre en minuscule permet de ne pas avoir 'S' avant 'a'
+			liste.push(ALL_PLAYERS[i][key].toLowerCase());
+		} else {
+			liste.push(ALL_PLAYERS[i][key]);
 		}
 	}
-	l.innerHTML = e;
-	l.style.textAlign = 'left';
+	if (key == 'nom') { // tri alphabétique
+		liste.sort();
+	} else {			// tri numérique
+		liste.sort(function (a, b) {
+			if (a < b) {
+				return -1;
+			} else if (a > b) {
+				return 1;
+			} else {
+				return 0;
+			}
+			});
+	}
+	//~ inversion du tri si nécessaire
+	if (asc == false) {
+		var max = liste.length;
+		var tmp = [];
+		for (var i = 0; i < max; i++ ) {
+			tmp.push(liste.pop());
+		}
+		liste = tmp;
+	}
+	//~ création d'une liste 'tmp' dans l'ordre de 'liste' (trié ci-dessus)
+	//~ contenant les éléments de ALL_PLAYERS
+	var tmp = [];
+	COPY_ALL_PLAYERS = ALL_PLAYERS;
+	for ( var i in liste ) {
+		tmp.push(one_of_all_players(key, liste[i]));
+	}
+	//~ on obtient finalement une liste de dictionnaires triée
+	//~ suivant une valeur (key) du dictionnaire
+	all_players_to_html(tmp);
+}
+
+function pop_one(l, index) {
+	var tmp = [];
+	for (var i in l) {
+		if ( i != index ) {
+			tmp.push(l[i]);
+		}
+	}
+	return tmp;
+}
+
+function one_of_all_players(key, elt) {
+	for ( var i in COPY_ALL_PLAYERS ) {
+		var cle = COPY_ALL_PLAYERS[i][key];
+		if (key == 'nom') {
+			//~ vu qu'on a mis en minuscule pour le tri
+			//~ il faut comparer en minuscule aussi
+			cle = cle.toLowerCase();
+		}
+		if (cle == elt) {
+			var r = COPY_ALL_PLAYERS[i];
+			COPY_ALL_PLAYERS = pop_one(COPY_ALL_PLAYERS, i);
+			return r;
+		}
+	}
+}
+
+function all_players_to_html(j) {
+	//~ message
+	e = $('btn_tri').innerHTML;
+	for (var i in j) {
+		var p = "onclick=invite(" + j[i].id + ",'" + j[i].nom + "');";
+		e += "<div class='lst_players'><img class='lst_players' src='./img/stats.png' onclick='get_stats(" + j[i].id + ")'></div><div class='player lst_players' id=" + j[i].id + " " + p + ">  " + j[i].nom + " ( " + j[i].elo + " )</div><div></div>";
+	}
+	clean_log(e);
+	$('log').style.textAlign = 'left';
+}
+
+function players_return(r) {
+	var j = JSON.parse(r);
+	ALL_PLAYERS = j;
+	//~ on en profite pour mettre à jour le elo du joueur
+	for (var i in j) {
+		if (j[i].nom == user_ID) {
+			try_set_local('elo', j[i].elo);
+		}	
+	}
+	all_players_to_html(j);
 }
 
 function news_return(r) {
