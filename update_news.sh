aecaf00a70dba7a6c30e99efc3d8a2101fb85e8d
Pierrick Lebourgeois <pierrick@trent.homelinux.org>
2015-07-04 22:22:31 +0200

    Affiche seulement la date (sans l'heure) des derniers commits
    Les 30 derniers sont affichés (au lieu de 20).

--- update_news.sh
+++ update_news.sh
@@ -1,5 +1,5 @@
 # construction du fichier HTML
-git log --no-merges --pretty='<div class="com_auteur">%aD</div><div class="ta_left"><p>%B</p></div>' -n 20 | sed -e 'a <br/>' > news.html
+git log --no-merges --pretty='<div class="com_auteur">%ad</div><div class="ta_left"><p>%B</p></div>' --date=short -n 30 | sed -e 'a <br/>'  > news.html
 
 # modification de la variable BREAKING_NEWS
 # il suffit d'incrémenter la variable pour que les utilisateurs voit '(*)' sur le bouton 'news'.
